40efe5c40226a7a7667a182a54001952e19e6b7f
gustavogss <gustavogss.dev@gmail.com>
2022-12-23 12:54:13 -0300

    Fatiando strings

--- strings.py
+++ strings.py
@@ -25,6 +25,7 @@ for letra in NOME:
 # %d - para substituir números 
 # %f - para substituir números de ponto flutuante 
 print()
+
 print('Olá, meu nome é %s tenho %d anos de idade, e trabalho como %s %s.' %(NOME, IDADE, PROFISSAO, STACK))
 
 # Com o método format temos diversas formas de trabalhar usando {}
@@ -47,3 +48,45 @@ PI = 3.14159
 print(f'O valor de PI é: {PI:.2f}') #Coloca duas casas após o ponto 3.14
 print(f'O valor de PI é: {PI:10.2f}') #Coloca um espaço em branco de dez caracteres antes do ponto
 
+print()
+# Fatiamento de Strings
+print(NOME[0]) # Pega a primeira letra na posição 0
+print(NOME[-1]) # Pega a última letra da string
+print(NOME[:5]) # Pega do inicio até o caracter 5
+print(NOME[3:]) # Pega a partir da posição 3 ao final da string
+print(NOME[3:6]) # Pega um pedaço da string da posição 3 a 6
+print(NOME[3:8:2]) # Pega um pedaço da string da posição 3 a 8 contando de 2 em 2
+print(NOME[:]) # Retorna a cópia da string
+print(NOME[::-1]) # Retorna a string espelhada e invertida
+print()
+
+# Strings de múltiplas linhas ou triplas f'''. Respeita a quebra de linhas, espaços e formatação do texto
+mensagem = f'''
+Meu nome é {NOME}.
+Tenho {IDADE} idade.
+    Sou {PROFISSAO}.
+E trabalho com {STACK}
+'''
+print(mensagem)
+
+print()
+
+print(
+    f'''
+    ============== MENU ================\n  
+    1 - Depositar
+    2 - Sacar
+    0 - Sair\n
+    =====================================
+    
+        Obrigado por usar nosso sistema!
+    '''
+)
+
+# \n pula uma linha
+
+
+
+
+ 
+ 
