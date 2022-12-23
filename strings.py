7ee1ed3288fa13784f311d5269b4b44c1f2216d1
gustavogss <gustavogss.dev@gmail.com>
2022-12-23 12:15:43 -0300

    Interpolação de strings

--- strings.py
+++ strings.py
@@ -1,5 +1,10 @@
-NOME = input('Digite um nome: ')
+NOME = input('Digite seu nome: ')
+IDADE = int(input('Digite sua idade: '))
+PROFISSAO = input('Digite sua profissão: ')
+STACK = input('Digite a stack que você trabalha: ')
+
 print()
+
 print(NOME.upper())
 print(NOME.lower())
 print(NOME.title()) # 1 Maisucula e o restante minuscula
@@ -14,4 +19,31 @@ print("_".join(NOME)) # "tipo de caracter" passando letra a letra, substitui o l
 for letra in NOME:
     print (letra, end="_")
 
+# Interpolação de strings (A desvantagens é que as variáveis tem que ser informadas em sequência)
+
+# %s - para substituir strings
+# %d - para substituir números 
+# %f - para substituir números de ponto flutuante 
+print()
+print('Olá, meu nome é %s tenho %d anos de idade, e trabalho como %s %s.' %(NOME, IDADE, PROFISSAO, STACK))
+
+# Com o método format temos diversas formas de trabalhar usando {}
+# sem informar a posição, mas passando a sequência exata das variáveis
+# informando as posições {3}
+# substituindo as variáveis pelos seus nomes
+# por meio de dicionarios .format(**pessoa)
+
+print('Olá, meu nome é {} tenho {} anos de idade, e trabalho como {} {}.' .format(NOME, IDADE, PROFISSAO, STACK))
+print('Olá, meu nome é {1} tenho {2} anos de idade, e trabalho como {3} {0}.' .format(STACK, NOME, IDADE, PROFISSAO))
+print('Olá, meu nome é {nome} tenho {idade} anos de idade, e trabalho como {profissao} {stack}.' .format(nome=NOME, idade=IDADE, profissao=PROFISSAO, stack=STACK))
+
+pessoa = {"nome":"Gustavo", "idade":47, "profissao":"Programador", "stack":"Python"}
+print('Olá, meu nome é {nome} tenho {idade} anos de idade, e trabalho como {profissao} {stack}.' .format(**pessoa))
+
+# Manipulando string com o f (É a forma mais atual de utilizarmos)
+print(f'Olá, meu nome é {NOME} tenho {IDADE} anos de idade, e trabalho como {PROFISSAO} {STACK}.')
+print()
+PI = 3.14159
+print(f'O valor de PI é: {PI:.2f}') #Coloca duas casas após o ponto 3.14
+print(f'O valor de PI é: {PI:10.2f}') #Coloca um espaço em branco de dez caracteres antes do ponto
 
